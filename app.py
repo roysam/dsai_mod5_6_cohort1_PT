27686da3dfbaf58fc36d0993e938440dc6b458a4
Roy Sam <roy.sam@live.com>
2025-05-13 13:17:16 +0000

    9

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/gemini",methods=["GET","POST"])
+def gemini():
+    return(render_template("gemini.html"))
+
 if __name__ == "__main__":
     app.run()
